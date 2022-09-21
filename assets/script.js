438ea3107e1aaa96c352100b52d327752199fd97
Naveed Mahmoudian <nmahmoudian@gmail.com>
2022-09-20 17:26:42 -0700

    Added variables and DOM elements

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+// DOM Elements & Variables
+citySearch = $('.citySearch');
+searchBtn = $('.searchBtn');
+searchHistoryContainer = $('.searchHistory');
+cityName = $('.cityName');
+cityTemp = $('.cityTemp');
+cityWind = $('.cityWind');
+cityHumidity = $('.cityHumidity');
+fiveDayContainer = $('.fiveDayContainer');
+
