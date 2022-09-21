dc5a7b3b6998339d1950a6fd189b74233f2d6e2d
Naveed Mahmoudian <nmahmoudian@gmail.com>
2022-09-20 17:45:26 -0700

    Working on search city button functionality

--- assets/script.js
+++ assets/script.js
@@ -2,9 +2,27 @@
 citySearch = $('.citySearch');
 searchBtn = $('.searchBtn');
 searchHistoryContainer = $('.searchHistory');
+cityHistory = [];
 cityName = $('.cityName');
 cityTemp = $('.cityTemp');
 cityWind = $('.cityWind');
 cityHumidity = $('.cityHumidity');
 fiveDayContainer = $('.fiveDayContainer');
 
+// Get Searched City Name
+searchBtn.click(searchCity);
+
+function searchCity() {
+    userCityName = citySearch.val();
+    addToHistory(userCityName);
+}
+
+function addToHistory(city) {
+    cityHistory.push(city);
+    for (i = 0; i < cityHistory.length; i++) {
+        searchHistoryEl = document.createElement('button');
+        searchHistoryEl.setAttribute('class', 'btn btn-secondary col-12');
+        searchHistoryEl.innerText = city;
+        searchHistoryContainer.append(searchHistoryEl);
+    }
+}
